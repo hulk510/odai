4381a6d67f820ad655b8c7b566231cc167cf9dce
hulk510 <dorian.51069@gmail.com>
2023-11-29 14:33:22 +0900

    chore: Prettier format

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -1,17 +1,17 @@
-import OpenAI from 'openai';
-import { GPTResponse } from '../../types/GPTResponse';
+import OpenAI from 'openai'
+import { GPTResponse } from '../../types/GPTResponse'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
 // MEMO: 何これ？
-export const runtime = 'edge';
+export const runtime = 'edge'
 
 type AIMessage = {
-  role: string;
-  content: string;
-};
+  role: string
+  content: string
+}
 
 const system: AIMessage = {
   role: 'system',
@@ -20,21 +20,21 @@ const system: AIMessage = {
   Rules: Your response should be concise, returning only the text of the topic. The topic should be unique, interesting, and relevant. json format example is { "topic": "topic text", choice1: "response 1", choice2: "response 2"}
   Note: Please respond in Japanese. Ensure that the topic is in the form of a question.
 `,
-};
+}
 
 // MEMO: requestに型をつけるには？zodでできないの？
 // Streamじゃなくていいかも
 // Jsonで返して、回答やお題、またお題の説明も考えてもらうといいかも。
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages } = await req.json()
   const response = await openai.chat.completions.create({
     model: 'gpt-4-1106-preview',
     response_format: { type: 'json_object' },
     messages: [system, messages],
-  });
+  })
 
   // TODO: try catchでエラーがあれば、エラーを返す
-  const content = JSON.parse(response.choices[0].message.content ?? '');
-  const r = GPTResponse.parse(content);
-  return Response.json(r);
+  const content = JSON.parse(response.choices[0].message.content ?? '')
+  const r = GPTResponse.parse(content)
+  return Response.json(r)
 }
